fdc139682f80000444030d8772011f5b2b3e53eb
Ramesh Kumar <prajapatiramesh18k>
2026-04-14 22:18:26 +0530

    comment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       <head>
         <link
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" //css
         />
         <link
           rel="stylesheet"
